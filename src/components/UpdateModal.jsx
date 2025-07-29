a039c107b3bd971d958f09d17cbc5d23a74388c6
prithu64 <prgthemessenger@gmail.com>
2025-07-29 20:35:19 +0530

    updt

--- src/components/UpdateModal.jsx
+++ src/components/UpdateModal.jsx
@@ -1,12 +1,48 @@
+import { useState } from "react";
 import FormSemiHeader from "./FormSemiHeader";
+import axios from "axios";
 
 
-function UpdateModal({modal,setModal}) {
+
+function UpdateModal({modal,setModal,refatch}) {
+  const [username,setUsername] = useState(null)
+  const [email,setEmail] = useState(null)
+  
+  const handleUpdate = async()=>{
+    const token = localStorage.getItem("token")
+    const payload = {}
+    if(username) payload.username = username;
+    if(email)  payload.email = email;
+    
+    if(Object.keys(payload).length===0){
+      alert("Atleast one input required")
+      return; 
+    }
+
+    try {
+        const response = await axios.post("http://localhost:3000/api/v1/user/updateuser",payload,
+       {
+        headers:{
+          authorization : `Bearer ${token}`
+        }
+       })
+      
+       setTimeout(()=>{
+         alert(response.data.data)
+       },1000)  
+       
+       refatch()
+    } catch (error) {
+      console.log("error :: ",error)
+      alert("Error updating the user")
+    }
+  }
+
 
   if(!modal)return null;  
   return (
     <div className={` ${
-        modal ? "opacity-100 scale-100 pointer-events-auto" : "opacity-0 scale-95 pointer-events-none"} w-full h-full fixed bg-black/50 backdrop-blur-sm  flex items-center justify-center z-50 inset-0  transition-all duration-300 ease-in-out `}>
+        modal ? "opacity-100 scale-100 pointer-events-auto" : "opacity-0 scale-95 pointer-events-none"} w-full h-full fixed text-white bg-black/50 backdrop-blur-sm  flex items-center justify-center z-50 inset-0  transition-all duration-300 ease-in-out `}>
          <div className="max-w-xl relative flex flex-col gap-y-4 ">
             <div onClick={()=>{setModal(false)}} className="absolute right-4 -top-6 cursor-pointer text-xl">
                 X
@@ -14,17 +50,17 @@ function UpdateModal({modal,setModal}) {
             <div className="flex flex-col gap-x-4 mx-auto">
                 <FormSemiHeader label={"Username"} />
                 <div className="flex ">
-                      <input placeholder="username" type="text" className="border rounded-md p-2 min-w-xs mr-3" />
+                   <input placeholder="username" onChange={(e)=>{setUsername(e.target.value)}} type="text" className="border rounded-md p-2 min-w-xs mr-3" />
                 </div>
             </div>
 
             <div className="flex flex-col gap-x-4 mx-auto">
                 <FormSemiHeader label={"email"} />
                 <div className="flex ">
-                  <input placeholder="email" type="text" className="border rounded-md p-2 min-w-xs mr-3" />
+                  <input placeholder="email" onChange={(e)=>{setEmail(e.target.value)}} type="email" className="border rounded-md p-2 min-w-xs mr-3" />
                 </div>
             </div>
-            <button className="border  mx-auto w-[140px] py-2">Update</button>
+            <button onClick={handleUpdate} className="border  mx-auto w-[140px] py-2">Update</button>
          </div>         
     </div>
   )
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,9 @@ import FormSemiHeader from "../components/FormSemiHeader"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 import UpdateModal from "../components/updateModal"
-
+import { TbPasswordUser } from "react-icons/tb";
+import { MdDelete } from "react-icons/md";
+import { FaPenToSquare } from "react-icons/fa6";
 
 
 function Profile() {
@@ -29,8 +31,12 @@ function Profile() {
  }
 
  const deleteUser = async()=>{
-    try {
+    const confirmDelete = window.confirm("Are you sure you want to delete your account ? ")
+   
+    if(confirmDelete){
+         try {
       const token = localStorage.getItem("token")
+
       await axios.delete("http://localhost:3000/api/v1/user/deleteuser",{
         headers : {
           authorization : `Bearer ${token}`
@@ -42,7 +48,9 @@ function Profile() {
     } catch (error) {
       console.log("error : ",error)
     }
+    }
  }
+
 
 
  useEffect(()=>{
@@ -54,39 +62,40 @@ function Profile() {
  },[user])
 
   return (
-    <div className='dark:bg-black/90 dark:text-white min-h-screen flex flex-col justify-center items-center'>
-      <h1>User Profile</h1>
+    <div className='dark:bg-black/90 dark:text-white min-h-screen flex flex-col justify-center items-center px-2'>
+     
       
-      <div>
-        <div className="flex flex-col gap-y-6">
+      <div className="border dark:border-white/40 px-3 py-6 rounded-md">
+         <h1 className=" text-xl text-center  mb-8 rounded-full">User Profile</h1>
+        <div className="flex flex-col gap-y-8 ">
             
-            <div className="flex flex-col gap-x-4 mx-auto">
+            <div className="flex flex-col gap-x-4 mx-auto ">
                 <FormSemiHeader label={"Username"} />
                 <div className="flex ">
-                      <input placeholder={user?.username || "Loading..."} readOnly={true} type="text" className="border rounded-md p-2 min-w-xs mr-3" />
+                      <input placeholder={user?.username || "Loading..."} readOnly={true} type="text" className="border rounded-md p-2 min-w-2xs " />
                 </div>
             </div>
 
             <div className="flex flex-col gap-x-4 mx-auto">
                 <FormSemiHeader label={"email"} />
                 <div className="flex ">
-                  <input placeholder={user?.email || "Loading..."} readOnly={true} type="text" className="border rounded-md p-2 min-w-xs mr-3" />
+                  <input placeholder={user?.email || "Loading..."} readOnly={true} type="text" className="border rounded-md p-2 min-w-2xs " />
                 </div>
             </div>
 
-            <div className="mx-auto space-x-2">
-                <button className="w-[150px] py-2 border dark:hover:bg-gray-600 transition-transform duration-200 cursor-pointer">
-                  Change Password
+            <div className="mx-auto space-x-4">
+                <button className=" px-3 py-2 border dark:hover:bg-gray-600 transition-transform duration-200 cursor-pointer">
+                  <TbPasswordUser size={20}/>
                 </button>
-                <button onClick={deleteUser}  className="w-[150px] py-2 border dark:hover:bg-gray-600 transition-transform duration-200 cursor-pointer">
-                  Delete User
+                <button onClick={deleteUser}  className="px-3 py-2 border dark:hover:bg-gray-600 transition-transform duration-200 cursor-pointer">
+                  <MdDelete size={20}/>
                 </button>
-                <button onClick={()=>setModal(true)} className="w-[150px] py-2 border dark:hover:bg-gray-600 transition-transform duration-200 cursor-pointer">
-                  Update
+                <button onClick={()=>setModal(true)} className="px-3 py-2 border dark:hover:bg-gray-600 transition-transform duration-200 cursor-pointer">
+                  <FaPenToSquare size={20}/>
                 </button>
             </div>
 
-             <UpdateModal modal={modal} setModal={setModal}/>
+             <UpdateModal refatch = {getUser} modal={modal} setModal={setModal}/>
              
             
         </div>
